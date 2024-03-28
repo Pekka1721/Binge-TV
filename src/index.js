b27f42e9bbdc48657fd1597233096283a670d3ea
Rajender Reddy <pekkarange1721@gmail.com>
2024-03-29 00:30:49 +0530

    middleware setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import conncectDB from "./db/db.js";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config()
 
-conncectDB();
+conncectDB()
+.then(()=>{
+    app.on('error',(error)=>{
+        console.log("failed to connect to db",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("App listening on PORT",process.env.PORT|| 8000);
+    })
+})
+.catch((error)=>{
+    console.log("error connecting to db:",error);
+})
 
 
 
