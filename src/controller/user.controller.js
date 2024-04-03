971bb112c7db1b82028d5ce56fd04932a324e613
Rajender Reddy <rajender.reddy@ampcus.com>
2024-04-03 16:48:45 +0530

    add user controller completed

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,9 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
 import  { ApiError } from '../utils/ApiError.js'
 import  User  from '../models/user.model.js'
+import uploadOnCloudinary from '../utils/cloudinary.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
+
 
 const registerUser = asyncHandler(async (req,res)=>{
     
@@ -12,7 +15,41 @@ const registerUser = asyncHandler(async (req,res)=>{
     ) {
         throw new ApiError(400,"All fields are mandatroy")
     }
-    User.findOne() 
+   const existedUser= User.findOne({
+        $or:[{ username },{ email }]
+    })
+    if(existedUser){
+        throw new ApiError(409,"Username is not available")
+    }
+    const avatartLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatartLocalPath){
+        throw new ApiError(400,"Avatar is required")
+    }
+    const avatar = await uploadOnCloudinary(avatartLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    if(!avatar){
+        throw new ApiError(400,"Avatar is required")
+    }
+    const user = await User.create({
+        fullname,
+        avatar:avatar.url,
+        coverImage:coverImage?.url||"",
+        email,
+        password,
+        username:username.toLowerCase()
+    })
+    const userCreated = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+    if(!userCreated){
+        throw new ApiError(500,"Something broke while registering user")
+    }
+    
+    return res.statusCode(200).json(
+        new ApiResponse(200,userCreated,"User Successfully Registerd")
+    )
 })
 
-export {registerUser};
+export {registerUser}
